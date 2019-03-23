1a244a9fc20075da64f9f99aa4cbaa22110174b3
Cristiam Acuña <cristiamacuna@gmail.com>
2019-03-22 18:16:02 -0600

    Initial Commit
    Web platform ready

---
+++ js/code.js
@@ -0,0 +1,39 @@
+var editor =CodeMirror(document.getElementById("codeeditor"),{
+  value: "#El código del archivo va aquí",
+  mode:  "python",
+  lineNumbers: true
+});
+
+function readFiles(event){
+  var files = event.target.files;
+  for (var i = 0, f; f = files[i]; i++)
+     {
+ 
+         var reader = new FileReader();
+         reader.onload = (function(reader)
+         {
+             return function()
+             {
+                 var contents = reader.result;
+                 var lines = contents.split('\n');
+                 //////
+                 editor.setValue(contents);
+                 $('#filemodal').modal('hide');
+                 $('#fileList').append('<li>'+files[0]['name']+'</li>');
+                
+             }
+         })(reader);
+ 
+         reader.readAsText(f);
+     }
+ }
+ document.getElementById('files').addEventListener('change', readFiles, false);
+
+
+ function getFileName(){
+
+ }
+
+ //Hacer domingo:
+ //Sacar los filenames de los multiples archivos (o solo 1)
+ //Poner en el explorador
